e373de15329cf5580a83378c58ef1e21f0f1d265
Batoul Alkarim <batoulalkarim1@gmail.com>
2022-02-03 20:48:22 -0500

    commit tree 9178853922e1c15c1d9f175c5ad0a43a93ae71f3

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+// Reduce returns the value of a prev calculation
+// If you have just one variable, it will return the first element of the array
+// If you have two variables, the first one stores the result of the callback function
+// the second one is lined up to be aggregated to the previous value (e.g added; see the example below)
+
+const totalBatteries = batteryBatches.reduce((prev, curr) => prev + curr)
+
+
+
